78b507dd1d02efad8196bcdeb855fce568e575dc
Georges Dupéron <georges.duperon@gmail.com>
2017-01-01 22:22:18 +0100

    Forgot to use the patched version of extend-reader in the previous commit

--- aful/reader.rkt
+++ aful/reader.rkt
@@ -62,14 +62,14 @@
 (require syntax/strip-context)
 (define ((wrap-reader-unhygienic p) . p-args)
   (strip-context
-   (apply (extend-reader p
-                         (λ ([orig-rt (current-readtable)]
-                             #:outer-scope outer-scope
-                             #:arg-str [arg-str (current-arg-string)])
-                           (make-aful-readtable orig-rt
-                                               #:outer-scope (λ (stx [mode 'flip]) stx)
-                                               #:arg-str arg-str))
-                         #:hygiene? #f)
+   (apply (extend-reader-unhygienic p
+                                    (λ ([orig-rt (current-readtable)]
+                                        #:outer-scope outer-scope
+                                        #:arg-str [arg-str (current-arg-string)])
+                                      (make-aful-readtable orig-rt
+                                                           #:outer-scope (λ (stx [mode 'flip]) stx)
+                                                           #:arg-str arg-str))
+                                    #:hygiene? #f)
           p-args)))
 
 (define (make-aful-readtable [orig-rt (current-readtable)]
